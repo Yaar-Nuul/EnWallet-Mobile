a8cff68c85099446b5d0452dc58562dee0a503aa
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-07-08 08:56:44 +0300

    add project

---
+++ app/src/main/java/com/akirachix/enwallet/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.akirachix.enwallet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.enwallet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvWallet.layoutManager = LinearLayoutManager(this)
+        displayEnwallet(walletItems = WalletItems())
+
+    }
+    fun displayEnwallet(walletItems: List<WalletItems>){
+        val salary = WalletItems("drawable/income_trt.xml", "Salary", "KES 24000", "4 July 2024")
+
+
+
+
+//        val contactsAdapter = BlogAdapter(contactList)
+//        binding.rvWallet.adapter = contactsAdapter
+    }
+}
---
+++ app/src/main/java/com/akirachix/enwallet/WalletItems.kt
@@ -0,0 +1,3 @@
+package com.akirachix.enwallet
+
+data class WalletItems(var icons: String, var itemName: String, var amount: String, var date: String )
